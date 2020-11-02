21b598576ba095ae8d1618aa1f53f6c6a0e00ca8
Andrew Johnston <42980380+andrewjohnston99@users.noreply.github.com>
2020-11-01 19:08:33 -0500

    7b in progress

--- phase3_shell.sql
+++ phase3_shell.sql
@@ -214,6 +214,24 @@ CREATE PROCEDURE test_sign_up(
 BEGIN
 -- Type solution below
 
+    SELECT username
+    INTO @curr_appointment_username
+    FROM appointment JOIN site on appointment.site_name = site.site_name
+    WHERE site.location = (SELECT location FROM student JOIN user ON username = student_username WHERE student_username = i_username)
+		AND appointment.site_name = i_site_name AND appt_date = i_appt_date AND appt_time = i_appt_time;
+
+    SELECT DISTINCT username
+    INTO @curr_appointment_pending_username
+    FROM appointment JOIN test ON appt_site = site_name AND test.appt_date = appointment.appt_date AND test.appt_time = appointment.appt_time
+    WHERE test_status = "pending";
+    
+    IF
+        (@curr_appointment_username IS NULL) AND (i_username NOT IN (@curr_appointment_pending_username))
+    THEN
+        UPDATE appointment
+        SET  username = i_username
+        WHERE site_name = i_site_name AND appt_date = i_appt_date AND appt_time = i_appt_time;
+    END IF;
 
 -- End of solution
 END //
